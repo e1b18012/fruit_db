e71c35929ac90ae4f1916e64ef29b44cf223751d
e1b18012 <e1b18012.oit.ac.jp>
2020-10-27 16:02:25 +0900

    Sample05の初期設定

---
+++ src/main/java/oit/is/lec05_9/fruit_db/FruitDbApplication.java
@@ -0,0 +1,13 @@
+package oit.is.lec05_9.fruit_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FruitDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FruitDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/lec05_9/fruit_db/FruitDbApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.lec05_9.fruit_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FruitDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
